a01791cb892db305497090b509e24cd0a4193de9
WhiteBlackGoose <wbg@angouri.org>
2021-10-21 23:35:50 +0300

    Trying to make asm syntax for F#...

---
+++ AsmToDelegate.FSharp/Library.fs
@@ -0,0 +1,35 @@
+﻿module AsmToDelegate.FSharp.Syntax
+
+open Iced.Intel
+open type Iced.Intel.AssemblerRegisters
+
+let inline mov (a : ^a0) (b : ^a1) (asm : ^a when ^a : (member mov : ^a0 * ^a1 -> unit)) =    
+    ((^a) : (member mov : ^a0 * ^a1 -> unit) (asm, a, b))
+
+let inline add (a : ^a0) (b : ^a1) (asm : ^a when ^a : (member add : ^a0 * ^a1 -> unit)) =    
+    ((^a) : (member add : ^a0 * ^a1 -> unit) (asm, a, b))
+
+let ret (asm : Assembler) =
+    asm.ret ()
+
+type AsmBuilder () =
+    member private this.Assembler : Assembler = Assembler 64
+
+    member inline this.Yield (m : ^a -> unit when ^a : (member mov : ^a0 * ^a1 -> unit)) =
+        m (this.Assembler :> obj :?> ^a)
+
+    member this.Delay(f) = f()
+
+    member this.Zero () =
+        ()
+
+    member this.Combine (a, b) =
+        this
+
+let asm = AsmBuilder ()
+
+asm {    
+    yield (mov rax rcx)
+    yield (add rax rdx)
+    yield ret
+} |> ignore
