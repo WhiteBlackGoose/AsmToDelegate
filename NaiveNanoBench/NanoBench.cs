426033ccf47fb5dd261efcd53fd855af84d83d31
WhiteBlackGoose <wbg@angouri.org>
2021-10-21 19:10:51 +0300

    Naive nano bench

---
+++ NaiveNanoBench/NanoBench.cs
@@ -0,0 +1,113 @@
+﻿using AsmToDelegate;
+using System.Diagnostics;
+using Plotly.NET;
+using HonkPerf.NET.RefLinq;
+
+namespace NaiveNanoBench;
+
+public sealed unsafe class NanoBench
+{
+    private readonly ulong warmupIterations;
+    private ulong warmupInvokationsPerIteration;
+    private readonly ulong actualIterations;
+    private readonly double timePerIteration;
+    private readonly Stopwatch sw = new();
+    private readonly List<double> results = new();
+
+    public NanoBench(ulong warmupIterations, ulong actualIterations, double timePerIteration, ulong defaultWarmupInvokationsPerIteration = 100_000)
+    {
+        this.warmupIterations = warmupIterations;
+        this.actualIterations = actualIterations;
+        this.timePerIteration = timePerIteration;
+        this.warmupInvokationsPerIteration = defaultWarmupInvokationsPerIteration;
+    }
+
+    private const ulong LoopUnroll = 32;
+    private (double Mean, double Total) MeasureMeanTime(delegate* unmanaged[Cdecl]<ulong, ulong> del, ulong invokations)
+    {
+        sw.Reset();
+        sw.Start();
+        for (ulong i = 0; i < invokations / LoopUnroll; i++)
+        {
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+            del(i); del(i); del(i); del(i);
+        }
+        sw.Stop();
+        var total = (double)sw.ElapsedMilliseconds / 1000;
+        var mean = total / (invokations / LoopUnroll * LoopUnroll);
+        return (mean, total);
+    }
+
+    private static string FormatTimePerInvoke(double time)
+    {
+        if (time >= 0.1)
+            return $"{time:0.###} s.";
+        if (time >= 0.000_1)
+            return $"{time * 1_000:0.###} ms.";
+        if (time >= 0.000_000_1)
+            return $"{time * 1_000_000:0.###} us.";
+        return $"{time * 1_000_000_000:0.###} ns.";
+    }
+
+    public void Bench(Extensions.UnmanagedFunctionWinX64<ulong, ulong> func)
+    {
+        results.Clear();
+        var del = func.Delegate;
+
+
+        Console.WriteLine("Warm up...");
+
+        var measuredSum = 0d;
+        for (ulong i = 0; i < warmupIterations; i++)
+        {
+            var (mean, total) = MeasureMeanTime(del, warmupInvokationsPerIteration);
+            if (total < 0.1)
+            {
+                warmupInvokationsPerIteration = 3 * warmupInvokationsPerIteration / 2;
+                Console.WriteLine($"Skipping {i + 1}, as {FormatTimePerInvoke(total)} is too little");
+                i--;
+                continue;
+            }
+            if (total > 10)
+            {
+                warmupInvokationsPerIteration = 2 * warmupInvokationsPerIteration / 3;
+                Console.WriteLine($"Skipping {i + 1}, as {FormatTimePerInvoke(total)} is too much");
+                i--;
+                continue;
+            }
+            measuredSum += mean;
+            Console.WriteLine($"Warm up {i + 1} / {warmupIterations}: {FormatTimePerInvoke(mean)} per invokation");
+        }
+
+        var avg = measuredSum / warmupIterations;
+        Console.WriteLine($"Average warmup time: {FormatTimePerInvoke(measuredSum)}");
+
+        var actualInvokationsPerIteration = (ulong)(timePerIteration / avg);
+        Console.WriteLine($"Actual invokations per iteration: {actualInvokationsPerIteration}");
+
+        Console.WriteLine("\nActual measuring...");
+
+        for (ulong i = 0; i < actualIterations; i++)
+        {
+            var (mean, _) = MeasureMeanTime(del, actualInvokationsPerIteration);
+            results.Add(mean);
+            Console.WriteLine($"Actual {i + 1} / {actualIterations}: {FormatTimePerInvoke(mean)} per invokation");
+        }
+
+        Console.WriteLine($"\nMean: {FormatTimePerInvoke(results.ToRefLinq().Average())}\n");
+    }
+
+    public IList<double> LastResults => results;
+
+    public void ShowDistributionOfResults()
+    {
+        var histo = Chart2D.Chart.Histogram<double, double, double>(results);
+        Chart.Show(histo);
+    }
+}
---
+++ NaiveNanoBench/Program.cs
@@ -0,0 +1,34 @@
+﻿using Iced.Intel;
+using static Iced.Intel.AssemblerRegisters;
+using AsmToDelegate;
+using System.Diagnostics;
+using NaiveNanoBench;
+
+unsafe
+{
+
+    var fourInstructions = new Assembler(bitness: 64);
+    for (int i = 0; i < 1000; i++)
+    {
+        fourInstructions.mov(rcx, 6);
+        fourInstructions.mov(r8, 10);
+        fourInstructions.add(eax, r8d);
+        fourInstructions.add(eax, ecx);
+    }
+    fourInstructions.ret();
+
+    var compiled = fourInstructions.ToUnmanagedFunctionWinX64<ulong, ulong>();
+
+    var dummy = new Assembler(bitness: 64);
+    dummy.ret();
+
+    var dummyCompiled = dummy.ToUnmanagedFunctionWinX64<ulong, ulong>();
+
+    var bench = new NanoBench(10, 100, 1.0);
+
+    bench.Bench(compiled);
+    bench.ShowDistributionOfResults();
+
+    bench.Bench(dummyCompiled);
+    bench.ShowDistributionOfResults();
+}
