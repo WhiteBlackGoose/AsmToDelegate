9921a31ff0be105a45b6f74f7e1504142bd5b4c7
WhiteBlackGoose <wbg@angouri.org>
2021-10-21 21:41:42 +0300

    Minor changes

--- NaiveNanoBench.Playground/Program.cs
+++ NaiveNanoBench.Playground/Program.cs
@@ -5,6 +5,24 @@ using NaiveNanoBench;
 
 unsafe
 {
+    var factorial = new Assembler(bitness: 64);
+    var label = factorial.CreateLabel();
+    factorial.mov(rax, 0);
+    factorial.mov(rcx, 1);
+    factorial.Label(ref label);
+    factorial.inc(rax);
+    factorial.imul(rcx, rax);
+    factorial.cmp(rax, 5);
+    factorial.jl(label);
+    factorial.mov(rax, rcx);
+    factorial.ret();
+    var compiled = factorial.ToUnmanagedFunctionWinX64<ulong, ulong>();
+    var benchmark = new NanoBench();
+    benchmark.Bench(compiled.Delegate);
+    // benchmark.Bench(&Factorial);
+    benchmark.ShowDistributionOfResults();
+
+
     // var movFunction = new Assembler(bitness: 64);
     // for (int i = 0; i < 1000; i++)
     // {
@@ -12,22 +30,28 @@ unsafe
     // }
     // movFunction.ret();
 
-    var movFunction = new Assembler(bitness: 64);
-    var label = movFunction.CreateLabel();
+    // Console.WriteLine(compiled.Delegate(3));
+    // 
+    // return;
 
-
-    var compiled = movFunction.ToUnmanagedFunctionWinX64<ulong, ulong>();
-
-    var dummy = new Assembler(bitness: 64);
-    dummy.ret();
-
-    var dummyCompiled = dummy.ToUnmanagedFunctionWinX64<ulong, ulong>();
-
-    var bench = new NanoBench(userInvokationsPerCall: 1000);
-
-    bench.Bench(compiled.Delegate);
+    // var dummy = new Assembler(bitness: 64);
+    // dummy.ret();
+    // 
+    // var dummyCompiled = dummy.ToUnmanagedFunctionWinX64<ulong, ulong>();
+    // 
+    // var bench = new NanoBench(userInvokationsPerCall: 1000);
+    // 
+    // bench.Bench(compiled.Delegate);
     // bench.ShowDistributionOfResults();
 
     // bench.Bench(dummyCompiled.Delegate);
     // bench.ShowDistributionOfResults();
 }
+
+// static ulong Factorial(ulong _)
+// {
+//     var res = 1ul;
+//     for (int i = 0; i < 5; i++)
+//         res *= i;
+//     return res;
+// }
